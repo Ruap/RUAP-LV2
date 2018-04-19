dd664072ab3850ac5ae87ba6bf58275c0ff1469d
unknown <thermalt4ke@gmail.com>
2018-04-19 10:08:54 +0200

    zad2

--- ContactManager/ContactManager/Services/ContactRepository.cs
+++ ContactManager/ContactManager/Services/ContactRepository.cs
@@ -6,25 +6,86 @@ using System.Web;
 
 namespace ContactManager.Services
 {
+
     public class ContactRepository
     {
+        private const string ChaceKey = "ContactStore";
+
+        public ContactRepository()
+        {
+            var ctx = HttpContext.Current;
+
+            if (ctx != null)
+            {
+                if(ctx.Cache[ChaceKey] == null)
+                {
+                    var contacts = new Contact[]
+                    {
+                        new Contact
+                        {
+                            Id = 1,
+                            Name = "David Task2"
+                        },
+
+                        new Contact
+                        {
+                            Id = 2,
+                            Name = "Mark Task2"
+                        }
+                    };
+
+                    ctx.Cache[ChaceKey] = contacts;
+                }
+            }
+        }
+
 
         public Contact[] GetAllContacts()
         {
+            var ctx = HttpContext.Current;
+
+            if (ctx != null)
+            {
+                return (Contact[])ctx.Cache[ChaceKey];
+            }
+
+
             return new Contact[]
             {
                 new Contact
                 {
-                    Id = 1,
-                    Name = "David Repository"
-                },
-                new Contact
-                {
-                    Id = 2,
-                    Name = "Mark Repository"
+                    Id = 0,
+                    Name = "Placeholder"
                 }
-
+           
             };
         }
+
+
+
+            public bool SaveContact(Contact contact)
+            {
+                var ctx = HttpContext.Current;
+
+                if(ctx != null)
+                {
+                   try{
+                       var currentData = ((Contact[])ctx.Cache[ChaceKey]).ToList();
+                       currentData.Add(contact);
+                       ctx.Cache[ChaceKey] = currentData.ToArray();
+
+                       return true;
+                   }
+                    catch(Exception ex)
+                   {
+                        Console.WriteLine(ex.ToString());
+                        return false;
+                    }
+                }
+                return false;
+            }
+
+
+        
     }
 }
