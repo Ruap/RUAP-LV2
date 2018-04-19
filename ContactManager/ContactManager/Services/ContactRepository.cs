e0c547a483352cdf32865221d752061c3442e231
unknown <thermalt4ke@gmail.com>
2018-04-19 09:47:24 +0200

    zad1

---
+++ ContactManager/ContactManager/Services/ContactRepository.cs
@@ -0,0 +1,30 @@
+﻿using ContactManager.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace ContactManager.Services
+{
+    public class ContactRepository
+    {
+
+        public Contact[] GetAllContacts()
+        {
+            return new Contact[]
+            {
+                new Contact
+                {
+                    Id = 1,
+                    Name = "David Repository"
+                },
+                new Contact
+                {
+                    Id = 2,
+                    Name = "Mark Repository"
+                }
+
+            };
+        }
+    }
+}
